72126ab148156ebe7ceeadc805ace17f4389bd4e
bonbon <nepunepubon@gmail.com>
2023-12-01 00:50:16 +0800

    huh

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     validates :name, presence: true, length: { minimum: 5, maximum: 255 }
+    has_many :tasks, dependent: :destroy
 end
--- db/migrate/20231130121742_create_tasks.rb
+++ db/migrate/20231130121742_create_tasks.rb
@@ -1,6 +1,6 @@
-class CreateCategories < ActiveRecord::Migration[7.0]
+class CreateTasks < ActiveRecord::Migration[7.0]
   def change
-    create_table :categories do |t|
+    create_table :tasks do |t|
       t.string :name
       t.string :details
 
